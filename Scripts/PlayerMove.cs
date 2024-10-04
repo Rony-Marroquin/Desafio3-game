ab4d9895ee87fa16e955dac3665d90ff69fccbf0
Rony-Marroquin <110794204+Rony-Marroquin@users.noreply.github.com>
2024-10-04 12:39:24 -0600

    Update PlayerMove.cs

--- Scripts/PlayerMove.cs
+++ Scripts/PlayerMove.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; // Necesario para cambiar de escena
+using UnityEngine.SceneManagement; 
 
 public class PlayerMove : MonoBehaviour
 {
     public float runSpeed = 7;
     public float rotationSpeed = 250;
     public Animator animator;
-    public AudioSource audioSource;  // Asigna tu AudioSource en el Inspector
+    public AudioSource audioSource;  
 
     private float x, y;
     public Rigidbody rb;
@@ -16,38 +16,38 @@ public class PlayerMove : MonoBehaviour
 
     public Transform groundcheck;
     public float groundDistance = 0.1f;
-    public LayerMask groundMask; // Corregido: typo 'grounfMask' por 'groundMask'
+    public LayerMask groundMask; 
     bool isGrounded;
 
     void Update()
     {
-        // Obtener las entradas del jugador
+        
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
 
-        // Movimiento y rotación
+      
         transform.Rotate(0, x * Time.deltaTime * rotationSpeed, 0);
         transform.Translate(0, 0, y * Time.deltaTime * runSpeed);
 
-        // Parámetros de movimiento en el Animator
+      
         animator.SetFloat("VeX", x);
         animator.SetFloat("VeY", y);
 
-        // Activar animación de baile
+      
         if (Input.GetKey("b"))
         {   
             animator.SetBool("baile", false);
             animator.Play("dance");  
         }
 
-        // Activar animación de golpe
+        
         if (Input.GetKey("m"))
         {   
             animator.SetBool("golpe", false);
             animator.Play("punch");  
         }
 
-        // Revertir animaciones si hay movimiento
+       
         if (x != 0 || y != 0)
         {
             animator.SetBool("baile", true);
@@ -55,13 +55,13 @@ public class PlayerMove : MonoBehaviour
             animator.SetBool("salto", true);
         }
 
-        // Comprobar si está en el suelo
+   
         isGrounded = Physics.CheckSphere(groundcheck.position, groundDistance, groundMask);
 
-        // Mostrar si el personaje está en el suelo en la consola (para depuración)
+
         Debug.Log("Is Grounded: " + isGrounded);
 
-        // Activar animación de salto
+      
        if (Input.GetKey(KeyCode.Space) && isGrounded)
                {
             animator.Play("Jump up");
@@ -70,14 +70,14 @@ public class PlayerMove : MonoBehaviour
        
     
 
-        // Cambiar a la escena del menú al presionar la tecla "Escape"
+       
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             SceneManager.LoadScene("Main-Menu"); 
         }
     }
 
-    // Detectar colisiones con objetos etiquetados
+ 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("fresa"))
